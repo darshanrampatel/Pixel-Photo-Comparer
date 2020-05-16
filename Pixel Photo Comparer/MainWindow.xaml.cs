cd553d6d99e0a27ccd93b8bb6f8b0dc2c3fbd068
Darshan <darshanp@outlook.com>
2020-05-16 19:28:40 +0100

    Intial working version

--- Pixel Photo Comparer/MainWindow.xaml.cs
+++ Pixel Photo Comparer/MainWindow.xaml.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Jpeg;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Directory = System.IO.Directory;
 
 namespace Pixel_Photo_Comparer
 {
@@ -12,16 +20,18 @@ namespace Pixel_Photo_Comparer
         public MainWindow()
         {
             InitializeComponent();
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
             MoveDuplicatePhotos();
             ProcessDuplicatePhotos();
         }
-        
+
         readonly static string folderPath = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), @"OneDrive\Pictures\Camera Roll");
         readonly static string duplicateFolderPath = Path.Combine(folderPath, @"Duplicates");
         const string burst = "BURST";
 
         void MoveDuplicatePhotos()
-        {            
+        {
             var files = Directory.EnumerateFiles(folderPath, "*.jpg");
 
             var groupedPictures = files
@@ -61,13 +71,92 @@ namespace Pixel_Photo_Comparer
             var groupedPictures = duplicateFiles
                 .Where(f => f.Contains($"_{burst}"))
                 .GroupBy(f => Path.GetFileNameWithoutExtension(f).Split("_")?.FirstOrDefault(s => s.StartsWith(burst))?.Substring(burst.Length))
-                .Select(g => new KeyValuePair<string, HashSet<string>>(g.Key, g.ToHashSet()))
+                .Select((g, idx) => new GroupedPhotos { Index = idx + 1, Key = g.Key, Duplicates = g.ToHashSet() })
                 .ToList();
 
             Debug.WriteLine($"Found {groupedPictures.Count} grouped files");
 
-            // now we can show a list of these groups (which should all have 2 pictures)
-            // and then show the two thumbnails side-by-side
+            // TODO check if any groupedPictures has more than 2 picturea
+
+            DuplicatesListView.ItemsSource = groupedPictures;
+            DuplicatesListView.SelectedItem = groupedPictures[0];
+            DuplicatesListView.Focus();
+        }
+
+        class GroupedPhotos
+        {
+            public int Index { get; set; }
+            public string Key { get; set; }
+            public string KeyDisplay => DateTime.ParseExact(Key, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture).ToString("yyyy-MM-ddTHH:mm:ss.fff"); // seems like the filenames refer to local time
+            public HashSet<string> Duplicates { get; set; }
+        }
+
+        private void DuplicatesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var selectedDuplicate = e.AddedItems[0] as GroupedPhotos;
+                DisposeFileStreams();
+                var ( lhBitmap, lhRotation) = LoadAndRotateImage(lhFileStream, selectedDuplicate.Duplicates.First());
+                var(rhBitmap, rhRotation) = LoadAndRotateImage(rhFileStream, selectedDuplicate.Duplicates.Last());
+                Application.Current.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Background, new System.Threading.ThreadStart(delegate
+                {
+                    //Update UI here
+                    LHImage.LayoutTransform = lhRotation;
+                    LHImage.Source = lhBitmap;
+                    RHImage.LayoutTransform = rhRotation;
+                    RHImage.Source = rhBitmap;
+                }));
+            }
+        }
+
+        FileStream lhFileStream;
+        FileStream rhFileStream;
+
+        void DisposeFileStreams()
+        {
+            if (lhFileStream != null)
+            {
+                lhFileStream.Close();
+                lhFileStream.Dispose();
+                lhFileStream = null;
+            }
+            if (rhFileStream != null)
+            {
+                rhFileStream.Close();
+                rhFileStream.Dispose();
+                rhFileStream = null;
+            }
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+        }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        private (BitmapImage, RotateTransform) LoadAndRotateImage(FileStream stream, string filePath)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            var bitmap = new BitmapImage();
+            stream = File.OpenRead(filePath);
+            var rotation = GetRotation(stream);
+            stream.Position = 0;
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.None;
+            bitmap.StreamSource = stream;
+            bitmap.EndInit();            
+            return (bitmap, new RotateTransform(rotation));
+        }
+
+        private double GetRotation(FileStream fileStream) // from https://stackoverflow.com/a/39840498/5024969
+        {
+            var directories = JpegMetadataReader.ReadMetadata(fileStream, new[] { new ExifReader() });
+            var subIfdDirectory = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
+            var orientation = subIfdDirectory?.GetInt16(ExifDirectoryBase.TagOrientation);
+            return orientation switch
+            {
+                6 => 90D,
+                3 => 180D,
+                8 => 270D,
+                _ => 0D,
+            };
         }
     }
 }
